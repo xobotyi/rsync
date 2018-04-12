7c14bbfea56fdfeb0253d78201a71b74940f2407
xobotyi <xog3@yandex.ru>
2018-04-12 13:37:26 +0300

    options descriptions;

--- src/Option.php
+++ src/Option.php
@@ -10,117 +10,450 @@ namespace xobotyi\rsync;
 
 class Option
 {
-    public const ACLS                = 'acls';
-    public const ADDRESS             = 'address';
-    public const APPEND              = 'append';
-    public const APPEND_VERIFY       = 'append-verify';
-    public const ARCHIVE             = 'archive';
-    public const BACKUP              = 'backup';
-    public const BACKUP_DIR          = 'backup-dir';
-    public const BLOCKING_IO         = 'blocking-io';
-    public const BLOCK_SIZE          = 'block-size';
-    public const BWLIMIT             = 'bwlimit';
-    public const CHECKSUM            = 'checksum';
-    public const CHMOD               = 'chmod';
-    public const COMPARE_DEST        = 'compare-dest';
-    public const COMPRESS            = 'compress';
-    public const COMPRESS_LEVEL      = 'compress-level';
-    public const CONTIMEOUT          = 'contimeout';
-    public const COPY_DEST           = 'copy-dest';
-    public const COPY_DEVICES        = 'copy-devices';
-    public const COPY_DIRLINKS       = 'copy-dirlinks';
-    public const COPY_LINKS          = 'copy-links';
-    public const COPY_UNSAFE_LINKS   = 'copy-unsafe-links';
-    public const CVS_EXCLUDE         = 'cvs-exclude';
-    public const DEL                 = 'del';
-    public const DELAY_UPDATES       = 'delay-updates';
-    public const DELETE              = 'delete';
-    public const DELETE_AFTER        = 'delete-after';
-    public const DELETE_BEFORE       = 'delete-before';
-    public const DELETE_DELAY        = 'delete-delay';
-    public const DELETE_DURING       = 'delete-during';
-    public const DELETE_EXCLUDED     = 'delete-excluded';
-    public const DEVICES             = 'devices';
-    public const DIRS                = 'dirs';
-    public const DRY_RUN             = 'dry-run';
-    public const EXCLUDE             = 'exclude';
-    public const EXCLUDE_FROM        = 'exclude-from';
-    public const EXECUTABILITY       = 'executability';
-    public const EXISTING            = 'existing';
-    public const FAKE_SUPER          = 'fake-super';
-    public const FILES_FROM          = 'files-from';
-    public const FILTER              = 'filter';
-    public const FORCE               = 'force';
-    public const FROM0               = 'from0';
-    public const FUZZY               = 'fuzzy';
-    public const GROUP               = 'group';
-    public const HARD_LINKS          = 'hard-links';
-    public const HELP                = 'help';
-    public const HUMAN_READABLE      = 'human-readable';
-    public const ICONV               = 'iconv';
-    public const IGNORE_ERRORS       = 'ignore-errors';
-    public const IGNORE_EXISTING     = 'ignore-existing';
-    public const IGNORE_TIMES        = 'ignore-times';
-    public const INCLUDE             = 'include';
-    public const INCLUDE_FROM        = 'include-from';
-    public const INPLACE             = 'inplace';
-    public const IPV4                = 'ipv4';
-    public const IPV6                = 'ipv6';
-    public const ITEMIZE_CHANGES     = 'itemize-changes';
-    public const KEEP_DIRLINKS       = 'keep-dirlinks';
-    public const LINKS               = 'links';
-    public const LINK_DEST           = 'link-dest';
-    public const LIST_ONLY           = 'list-only';
-    public const LOG_FILE            = 'log-file';
-    public const LOG_FILE_FORMAT     = 'log-file-format';
-    public const MAX_DELETE          = 'max-delete';
-    public const MAX_SIZE            = 'max-size';
-    public const MIN_SIZE            = 'min-size';
-    public const MODIFY_WINDOW       = 'modify-window';
-    public const NO_IMPLIED_DIRS     = 'no-implied-dirs';
-    public const NO_MOTD             = 'no-motd';
-    public const NO_OPTION           = 'no-OPTION';
-    public const NUMERIC_IDS         = 'numeric-ids';
-    public const OMIT_DIR_TIMES      = 'omit-dir-times';
-    public const ONE_FILE_SYSTEM     = 'one-file-system';
-    public const ONLY_WRITE_BATCH    = 'only-write-batch';
-    public const OUTPUT_8_BIT        = '8-bit-output';
-    public const OUT_FORMAT          = 'out-format';
-    public const OWNER               = 'owner';
-    public const PARTIAL             = 'partial';
-    public const PARTIAL_DIR         = 'partial-dir';
-    public const PASSWORD_FILE       = 'password-file';
-    public const PERMS               = 'perms';
-    public const PORT                = 'port';
-    public const PROGRESS            = 'progress';
-    public const PROTECT_ARGS        = 'protect-args';
-    public const PROTOCOL            = 'protocol';
-    public const PRUNE_EMPTY_DIRS    = 'prune-empty-dirs';
-    public const QUIET               = 'quiet';
-    public const READ_BATCH          = 'read-batch';
-    public const RECURSIVE           = 'recursive';
-    public const RELATIVE            = 'relative';
+    /**
+     * preserve ACLs (implies --perms)
+     */
+    public const ACLS = 'acls';
+    /**
+     * bind address for outgoing socket to daemon
+     */
+    public const ADDRESS = 'address';
+    /**
+     * append data onto shorter files
+     */
+    public const APPEND = 'append';
+    /**
+     * like --append, but with old data in file checksum
+     */
+    public const APPEND_VERIFY = 'append-verify';
+    /**
+     * archive mode; equals -rlptgoD (no -H,-A,-X)
+     */
+    public const ARCHIVE = 'archive';
+    /**
+     * make backups (see --suffix & --backup-dir)
+     */
+    public const BACKUP = 'backup';
+    /**
+     * make backups into hierarchy based in DIR
+     */
+    public const BACKUP_DIR = 'backup-dir';
+    /**
+     * use blocking I/O for the remote shell
+     */
+    public const BLOCKING_IO = 'blocking-io';
+    /**
+     * force a fixed checksum block-size
+     */
+    public const BLOCK_SIZE = 'block-size';
+    /**
+     * limit I/O bandwidth; KBytes per second
+     */
+    public const BWLIMIT = 'bwlimit';
+    /**
+     * skip based on checksum, not mod-time & size
+     */
+    public const CHECKSUM = 'checksum';
+    /**
+     * affect file and/or directory permissions
+     */
+    public const CHMOD = 'chmod';
+    /**
+     * compare destination files relative to DIR
+     */
+    public const COMPARE_DEST = 'compare-dest';
+    /**
+     * compress file data during the transfer
+     */
+    public const COMPRESS = 'compress';
+    /**
+     * explicitly set compression level
+     */
+    public const COMPRESS_LEVEL = 'compress-level';
+    /**
+     * set daemon connection timeout in seconds
+     */
+    public const CONTIMEOUT = 'contimeout';
+    /**
+     * include copies of unchanged files
+     */
+    public const COPY_DEST = 'copy-dest';
+    /**
+     * copy device contents as regular file
+     */
+    public const COPY_DEVICES = 'copy-devices';
+    /**
+     * transform symlink to a dir into referent dir
+     */
+    public const COPY_DIRLINKS = 'copy-dirlinks';
+    /**
+     * transform symlink into referent file/dir
+     */
+    public const COPY_LINKS = 'copy-links';
+    /**
+     * only "unsafe" symlinks are transformed
+     */
+    public const COPY_UNSAFE_LINKS = 'copy-unsafe-links';
+    /**
+     * auto-ignore files the same way CVS does
+     */
+    public const CVS_EXCLUDE = 'cvs-exclude';
+    /**
+     * an alias for --delete-during
+     */
+    public const DEL = 'del';
+    /**
+     * put all updated files into place at transfer's end
+     */
+    public const DELAY_UPDATES = 'delay-updates';
+    /**
+     * delete extraneous files from destination dirs
+     */
+    public const DELETE = 'delete';
+    /**
+     * receiver deletes after transfer, not during
+     */
+    public const DELETE_AFTER = 'delete-after';
+    /**
+     * receiver deletes before transfer, not during
+     */
+    public const DELETE_BEFORE = 'delete-before';
+    /**
+     * find deletions during, delete after
+     */
+    public const DELETE_DELAY = 'delete-delay';
+    /**
+     * receiver deletes during transfer (default)
+     */
+    public const DELETE_DURING = 'delete-during';
+    /**
+     * delete excluded files from destination dirs
+     */
+    public const DELETE_EXCLUDED = 'delete-excluded';
+    /**
+     * preserve device files (super-user only)
+     */
+    public const DEVICES = 'devices';
+    /**
+     * transfer directories without recursing
+     */
+    public const DIRS = 'dirs';
+    /**
+     * perform a trial run with no changes made
+     */
+    public const DRY_RUN = 'dry-run';
+    /**
+     * exclude files matching PATTERN
+     */
+    public const EXCLUDE = 'exclude';
+    /**
+     * read exclude patterns from FILE
+     */
+    public const EXCLUDE_FROM = 'exclude-from';
+    /**
+     * preserve the file's executability
+     */
+    public const EXECUTABILITY = 'executability';
+    /**
+     * skip creating new files on receiver
+     */
+    public const EXISTING = 'existing';
+    /**
+     * store/recover privileged attrs using xattrs
+     */
+    public const FAKE_SUPER = 'fake-super';
+    /**
+     * read list of source-file names from FILE
+     */
+    public const FILES_FROM = 'files-from';
+    /**
+     * add a file-filtering RULE
+     */
+    public const FILTER = 'filter';
+    /**
+     * force deletion of directories even if not empty
+     */
+    public const FORCE = 'force';
+    /**
+     * all *-from/filter files are delimited by 0s
+     */
+    public const FROM0 = 'from0';
+    /**
+     * find similar file for basis if no dest file
+     */
+    public const FUZZY = 'fuzzy';
+    /**
+     * preserve group
+     */
+    public const GROUP = 'group';
+    /**
+     * preserve hard links
+     */
+    public const HARD_LINKS = 'hard-links';
+    /**
+     * show help
+     */
+    public const HELP = 'help';
+    /**
+     * output numbers in a human-readable format
+     */
+    public const HUMAN_READABLE = 'human-readable';
+    /**
+     * request charset conversion of filenames
+     */
+    public const ICONV = 'iconv';
+    /**
+     * delete even if there are I/O errors
+     */
+    public const IGNORE_ERRORS = 'ignore-errors';
+    /**
+     * skip updating files that already exist on receiver
+     */
+    public const IGNORE_EXISTING = 'ignore-existing';
+    /**
+     * don't skip files that match in size and mod-time
+     */
+    public const IGNORE_TIMES = 'ignore-times';
+    /**
+     * don't exclude files matching PATTERN
+     */
+    public const INCLUDE      = 'include';
+    /**
+     * read include patterns from FILE
+     */
+    public const INCLUDE_FROM = 'include-from';
+    /**
+     * update destination files in-place
+     */
+    public const INPLACE = 'inplace';
+    /**
+     * prefer IPv4
+     */
+    public const IPV4 = 'ipv4';
+    /**
+     * prefer IPv6
+     */
+    public const IPV6 = 'ipv6';
+    /**
+     * output a change-summary for all updates
+     */
+    public const ITEMIZE_CHANGES = 'itemize-changes';
+    /**
+     * treat symlinked dir on receiver as dir
+     */
+    public const KEEP_DIRLINKS = 'keep-dirlinks';
+    /**
+     * copy symlinks as symlinks
+     */
+    public const LINKS = 'links';
+    /**
+     * hardlink to files in DIR when unchanged
+     */
+    public const LINK_DEST = 'link-dest';
+    /**
+     * list the files instead of copying them
+     */
+    public const LIST_ONLY = 'list-only';
+    /**
+     * log what we're doing to the specified FILE
+     */
+    public const LOG_FILE = 'log-file';
+    /**
+     * log updates using the specified FMT
+     */
+    public const LOG_FILE_FORMAT = 'log-file-format';
+    /**
+     * don't delete more than NUM files
+     */
+    public const MAX_DELETE = 'max-delete';
+    /**
+     * don't transfer any file larger than SIZE
+     */
+    public const MAX_SIZE = 'max-size';
+    /**
+     * don't transfer any file smaller than SIZE
+     */
+    public const MIN_SIZE = 'min-size';
+    /**
+     * compare mod-times with reduced accuracy
+     */
+    public const MODIFY_WINDOW = 'modify-window';
+    /**
+     * don't send implied dirs with --relative
+     */
+    public const NO_IMPLIED_DIRS = 'no-implied-dirs';
+    /**
+     * suppress daemon-mode MOTD
+     */
+    public const NO_MOTD = 'no-motd';
+    /**
+     * turn off an implied OPTION (e.g. --no-D)
+     */
+    public const NO_OPTION = 'no-OPTION';
+    /**
+     * don't map uid/gid values by user/group name
+     */
+    public const NUMERIC_IDS = 'numeric-ids';
+    /**
+     * omit directories from --times
+     */
+    public const OMIT_DIR_TIMES = 'omit-dir-times';
+    /**
+     * don't cross filesystem boundaries
+     */
+    public const ONE_FILE_SYSTEM = 'one-file-system';
+    /**
+     * like --write-batch but w/o updating destination
+     */
+    public const ONLY_WRITE_BATCH = 'only-write-batch';
+    /**
+     * leave high-bit chars unescaped in output
+     */
+    public const OUTPUT_8_BIT = '8-bit-output';
+    /**
+     * output updates using the specified FORMAT
+     */
+    public const OUT_FORMAT = 'out-format';
+    /**
+     * preserve owner (super-user only)
+     */
+    public const OWNER = 'owner';
+    /**
+     * keep partially transferred files
+     */
+    public const PARTIAL = 'partial';
+    /**
+     * put a partially transferred file into DIR
+     */
+    public const PARTIAL_DIR = 'partial-dir';
+    /**
+     * read daemon-access password from FILE
+     */
+    public const PASSWORD_FILE = 'password-file';
+    /**
+     * preserve permissions
+     */
+    public const PERMS = 'perms';
+    /**
+     * specify double-colon alternate port number
+     */
+    public const PORT = 'port';
+    /**
+     * show progress during transfer
+     */
+    public const PROGRESS = 'progress';
+    /**
+     * no space-splitting; only wildcard special-chars
+     */
+    public const PROTECT_ARGS = 'protect-args';
+    /**
+     * force an older protocol version to be used
+     */
+    public const PROTOCOL = 'protocol';
+    /**
+     * prune empty directory chains from the file-list
+     */
+    public const PRUNE_EMPTY_DIRS = 'prune-empty-dirs';
+    /**
+     * suppress non-error messages
+     */
+    public const QUIET = 'quiet';
+    /**
+     * read a batched update from FILE
+     */
+    public const READ_BATCH = 'read-batch';
+    /**
+     * recurse into directories
+     */
+    public const RECURSIVE = 'recursive';
+    /**
+     * use relative path names
+     */
+    public const RELATIVE = 'relative';
+    /**
+     * sender removes synchronized files (non-dirs)
+     */
     public const REMOVE_SOURCE_FILES = 'remove-source-files';
-    public const RSH                 = 'rsh';
-    public const RSYNC_PATH          = 'rsync-path';
-    public const SAFE_LINKS          = 'safe-links';
-    public const SIZE_ONLY           = 'size-only';
-    public const SKIP_COMPRESS       = 'skip-compress';
-    public const SOCKOPTS            = 'sockopts';
-    public const SPARSE              = 'sparse';
-    public const SPECIALS            = 'specials';
-    public const STATS               = 'stats';
-    public const SUFFIX              = 'suffix';
-    public const SUPER               = 'super';
-    public const TEMP_DIR            = 'temp-dir';
-    public const TIMEOUT             = 'timeout';
-    public const TIMES               = 'times';
-    public const UPDATE              = 'update';
-    public const VERBOSE             = 'verbose';
-    public const VERSION             = 'version';
-    public const WHOLE_FILE          = 'whole-file';
-    public const WRITE_BATCH         = 'write-batch';
-    public const XATTRS              = 'xattrs';
+    /**
+     * specify the remote shell to use
+     */
+    public const RSH = 'rsh';
+    /**
+     * specify the rsync to run on the remote machine
+     */
+    public const RSYNC_PATH = 'rsync-path';
+    /**
+     * ignore symlinks that point outside the source tree
+     */
+    public const SAFE_LINKS = 'safe-links';
+    /**
+     * skip files that match in size
+     */
+    public const SIZE_ONLY = 'size-only';
+    /**
+     * skip compressing files with a suffix in LIST
+     */
+    public const SKIP_COMPRESS = 'skip-compress';
+    /**
+     * specify custom TCP options
+     */
+    public const SOCKOPTS = 'sockopts';
+    /**
+     * handle sparse files efficiently
+     */
+    public const SPARSE = 'sparse';
+    /**
+     * preserve special files
+     */
+    public const SPECIALS = 'specials';
+    /**
+     * give some file-transfer stats
+     */
+    public const STATS = 'stats';
+    /**
+     * set backup suffix (default ~ w/o --backup-dir)
+     */
+    public const SUFFIX = 'suffix';
+    /**
+     * receiver attempts super-user activities
+     */
+    public const SUPER = 'super';
+    /**
+     * create temporary files in directory DIR
+     */
+    public const TEMP_DIR = 'temp-dir';
+    /**
+     * set I/O timeout in seconds
+     */
+    public const TIMEOUT = 'timeout';
+    /**
+     * preserve modification times
+     */
+    public const TIMES = 'times';
+    /**
+     * skip files that are newer on the receiver
+     */
+    public const UPDATE = 'update';
+    /**
+     * increase verbosity
+     */
+    public const VERBOSE = 'verbose';
+    /**
+     * print version number
+     */
+    public const VERSION = 'version';
+    /**
+     * copy files whole (without delta-xfer algorithm)
+     */
+    public const WHOLE_FILE = 'whole-file';
+    /**
+     * write a batched update to FILE
+     */
+    public const WRITE_BATCH = 'write-batch';
+    /**
+     * preserve extended attributes
+     */
+    public const XATTRS = 'xattrs';
 
     private const OPTIONS_LIST = [
         self::ACLS                => ['option' => 'acls', 'hasArgument' => false],
